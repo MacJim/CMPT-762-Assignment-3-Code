d050ee7c6df4cc216511e0566b1059dbe30e7c71
Jim Shi <shiyilin2008@gmail.com>
2021-02-23 22:38:21 -0800

    Adds some shared constants.

---
+++ constant.py
@@ -0,0 +1,11 @@
+import typing
+
+
+DATASET_ROOT_DIR: typing.Final = "data/"
+
+TRAIN_DATASET_SUB_DIR: typing.Final = "train/"
+TEST_DATASET_SUB_DIR: typing.Final = "test/"
+
+TRAIN_JSON_FILENAME: typing.Final = "train.json"
+DESIGNATED_CATEGORY_ID: typing.Final = 4
+DESIGNATED_CATEGORY_NAME: typing.Final = "plane"
