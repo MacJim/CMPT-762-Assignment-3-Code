bc276820c767d718eeb15c855adb8a7675e28dac
Jim Shi <shiyilin2008@gmail.com>
2021-03-04 01:08:35 -0800

    Object detection: Modifies the `get_config` function to return the baseline config.

--- object_detection_train.py
+++ object_detection_train.py
@@ -3,46 +3,66 @@ import os
 
 import detectron2.config
 from detectron2 import model_zoo
+from detectron2.engine import DefaultTrainer
 
 import constant.detectron
 
 
-# MARK: - Constants
-MODEL_YML = "COCO-Detection/faster_rcnn_R_101_FPN_3x.yaml"
-OUTPUT_DIR = "checkpoints"
+def _create_output_dir(dir_name: str):
+    if (os.path.exists(dir_name)):
+        if (os.path.isdir(dir_name)):
+            print(f"Using existing output dir {dir_name}")
+            print(f"Files in existing output dir may be overwritten.")
+        else:
+            raise FileExistsError(f"Output dir {dir_name} is a file or link!")
+    else:
+        os.makedirs(dir_name)
+        print(f"Created output dir {dir_name}")
 
 
 # MARK: - Config
-def update_config():
+def get_baseline_config() -> detectron2.config.config.CfgNode:
     """
+    Get the baseline config in our project handout.
+
     Source: https://detectron2.readthedocs.io/en/latest/modules/config.html
     """
-    cfg = detectron2.config.get_cfg()
+    model_yml = "COCO-Detection/faster_rcnn_R_101_FPN_3x.yaml"
+    output_dir = "output"
 
-    cfg.merge_from_file(model_zoo.get_config_file(MODEL_YML))
-    cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url(MODEL_YML)
+    _create_output_dir(output_dir)
+
+    cfg = detectron2.config.get_cfg()    # This is just a copy of the default config.
+
+    cfg.merge_from_file(model_zoo.get_config_file(model_yml))
+    # cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url(model_yml)    # I don't think I should load the weights in the baseline.
 
     cfg.DATASETS.TRAIN = (constant.detectron.TRAIN_DATASET_NAME,)
     cfg.DATASETS.TEST = (constant.detectron.TEST_DATASET_NAME,)
     cfg.DATALOADER.NUM_WORKERS = 4
 
-    cfg.SOLVER.IMS_PER_BATCH = 4    # Batch size: images per batch
+    cfg.SOLVER.IMS_PER_BATCH = 2    # Batch size: images per batch
     cfg.SOLVER.BASE_LR = 0.00025
-    cfg.SOLVER.MAX_ITER = 400
-    cfg.SOLVER.STEPS = []    # TODO: Learning rate scheduling
+    cfg.SOLVER.MAX_ITER = 500
+    # cfg.SOLVER.STEPS = []    # Learning rate scheduling: none in the baseline
 
-    cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 256    # Number of regions per image used to train RPN (Region Proposal Network)
+    cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 512    # Number of regions per image used to train RPN (Region Proposal Network)
     cfg.MODEL.ROI_HEADS.NUM_CLASSES = 1    # We only have a single plane class
 
-    cfg.OUTPUT_DIR = OUTPUT_DIR
+    cfg.OUTPUT_DIR = output_dir
+
+    return cfg
 
 
 # MARK: - Main
 def main():
-    # 1. Verify parameters
+    # Get config.
+    cfg = get_baseline_config()
 
-    # 2. Initialize config
-    update_config()
+    # Train.
+    trainer = DefaultTrainer(cfg)
+    trainer.resume_or_load(resume=False)    # Start from iteration 0.
+    trainer.train()    # Run training.
 
 
 if __name__ == '__main__':
