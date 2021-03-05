b1bc6b43f14aea59c78a96547c75527281916b2b
Jim Shi <shiyilin2008@gmail.com>
2021-03-04 16:58:41 -0800

    [Minor] The get baseline config function now forces the existence of the output folder in inference mode.

--- object_detection_config.py
+++ object_detection_config.py
@@ -3,6 +3,7 @@ Get Detectron configuration (`detectron2.config.config.CfgNode`).
 """
 
 import os
+import typing
 
 import detectron2.config
 from detectron2 import model_zoo
@@ -23,21 +24,31 @@ def _create_output_dir(dir_name: str):
         print(f"Created output dir `{dir_name}`")
 
 
-# MARK: - Config
-def get_baseline_config() -> detectron2.config.config.CfgNode:
+# MARK: - Baseline config
+BASELINE_MODEL_YML: typing.Final = "COCO-Detection/faster_rcnn_R_101_FPN_3x.yaml"
+BASELINE_OUTPUT_DIR: typing.Final = "output"
+
+
+def get_baseline_config(train=True) -> detectron2.config.config.CfgNode:
     """
     Get the baseline config in our project handout.
 
-    Source: https://detectron2.readthedocs.io/en/latest/modules/config.html
+    Source: https://detectron2.readthedocs.io/en/latest/modules/config.htmlx
     """
-    model_yml = "COCO-Detection/faster_rcnn_R_101_FPN_3x.yaml"
-    output_dir = "output"
+    if (train):
+        # Train: create the output dir.
+        _create_output_dir(BASELINE_OUTPUT_DIR)
+    else:
+        # Inference: the output dir must already exist.
+        if (not os.path.isdir(BASELINE_OUTPUT_DIR)):
+            raise FileNotFoundError(f"Output dir `{BASELINE_OUTPUT_DIR}` doesn't exist!")
+        else:
+            print(f"Using output dir `{BASELINE_OUTPUT_DIR}`")
 
-    _create_output_dir(output_dir)
-
+    # Create configuration.
     cfg = detectron2.config.get_cfg()    # This is just a copy of the default config.
 
-    cfg.merge_from_file(model_zoo.get_config_file(model_yml))
+    cfg.merge_from_file(model_zoo.get_config_file(BASELINE_MODEL_YML))
     # cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url(model_yml)    # I don't think I should load the weights in the baseline.
 
     cfg.DATASETS.TRAIN = (constant.detectron.TRAIN_DATASET_NAME,)
@@ -52,6 +63,6 @@ def get_baseline_config() -> detectron2.config.config.CfgNode:
     cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 512    # Number of regions per image used to train RPN (Region Proposal Network)
     cfg.MODEL.ROI_HEADS.NUM_CLASSES = 1    # We only have a single plane class
 
-    cfg.OUTPUT_DIR = output_dir
+    cfg.OUTPUT_DIR = BASELINE_OUTPUT_DIR
 
     return cfg
