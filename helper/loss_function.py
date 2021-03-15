9069b42f129cd5ac514a5c324471abbff19a9421
Jim Shi <shiyilin2008@gmail.com>
2021-03-14 20:46:54 -0700

    [Minor] Segmentation dice loss function: Removes the `view` function since the input and target tensors are of the same shape.

--- helper/loss_function.py
+++ helper/loss_function.py
@@ -4,8 +4,8 @@ import torch
 def dice_loss(input_tensor: torch.Tensor, target_tensor: torch.Tensor):
     smooth = 1.
 
-    i_flat = input_tensor.view(-1)
-    t_flat = target_tensor.view(-1)
-    intersection = (i_flat * t_flat).sum()
+    # i_flat = input_tensor.view(-1)
+    # t_flat = target_tensor.view(-1)
+    intersection = (input_tensor * target_tensor).sum()
 
-    return 1 - ((2. * intersection + smooth) / (i_flat.sum() + t_flat.sum() + smooth))
+    return 1 - ((2. * intersection + smooth) / (input_tensor.sum() + target_tensor.sum() + smooth))
