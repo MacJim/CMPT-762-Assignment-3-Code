68a01d20e65e4ff21585821e4be47f299963cca0
Jim Shi <shiyilin2008@gmail.com>
2021-03-16 01:24:03 -0700

    Segmentation: UNet inference: Adds an IoU calculation script (as required by the assignment).

--- segmentation_infer_unet.py
+++ segmentation_infer_unet.py
@@ -11,21 +11,30 @@ from dataset import register_datasets, get_detection_data
 from segmentation_dataset import PlaneDataset
 
 
+# MARK: - Constants
 N_CLASSES: typing.Final = 1
 INPUT_WIDTH: typing.Final = 256
 INPUT_HEIGHT: typing.Final = 256
-CHECKPOINT_FILENAME: typing.Final = "seg_output/140.pth"
-SAVE_DIR: typing.Final = "/tmp/seg_inference"
+CHECKPOINT_FILENAME: typing.Final = "../762A3-Checkpoints/seg_output_with_input_normalization/200.pth"
 
 
-def main():
-    if (not os.path.exists(SAVE_DIR)):
-        os.makedirs(SAVE_DIR)
+# MARK: - Network
+network = segmentation_unet.NestedUNet(N_CLASSES)
+network = network.cuda()
+network.load_state_dict(torch.load(CHECKPOINT_FILENAME))
+network.eval()
 
-    network = segmentation_unet.NestedUNet(N_CLASSES)
-    network = network.cuda()
-    network.load_state_dict(torch.load(CHECKPOINT_FILENAME))
-    network.eval()
+
+# MARK: - Training & validation data
+VISUALIZATION_SAVE_DIR: typing.Final = "/tmp/seg_inference"
+
+
+def visualize_training_patches():
+    """
+    Visualize 10 training/validation patches.
+    """
+    if (not os.path.exists(VISUALIZATION_SAVE_DIR)):
+        os.makedirs(VISUALIZATION_SAVE_DIR)
 
     register_datasets()
     train_and_val_info_dicts = get_detection_data("train")
@@ -40,6 +49,7 @@ def main():
 
         with torch.no_grad():
             prediction = network(image_cuda)
+            prediction = torch.sigmoid(prediction)
 
         # Remove batch dimension.
         image = torch.squeeze(image, 0)
@@ -51,13 +61,49 @@ def main():
         prediction = torch.sigmoid(prediction)
         prediction *= 255.0
 
-        save_image(image, os.path.join(SAVE_DIR, f"{i}-image.png"))
-        save_image(mask, os.path.join(SAVE_DIR, f"{i}-mask.png"))
-        save_image(prediction, os.path.join(SAVE_DIR, f"{i}-prediction.png"))
+        save_image(image, os.path.join(VISUALIZATION_SAVE_DIR, f"{i}-image.png"))
+        save_image(mask, os.path.join(VISUALIZATION_SAVE_DIR, f"{i}-mask.png"))
+        save_image(prediction, os.path.join(VISUALIZATION_SAVE_DIR, f"{i}-prediction.png"))
+
+
+def calculate_mean_iou():
+    """
+    Calculate training/validation mean IoU.
+    """
+    register_datasets()
+    train_and_val_info_dicts = get_detection_data("train")
+    train_and_val_dataset = PlaneDataset("train", train_and_val_info_dicts, INPUT_WIDTH, INPUT_HEIGHT, preload_images=False)
+    # Don't use data loader workers when `preload_images=False`.
+    # No I don't batch here because I want to check the IoU for each patch.
+    data_loader = data.DataLoader(train_and_val_dataset, shuffle=False)
+
+    ious = []
+
+    for image, mask in data_loader:
+        image = image.cuda()
+        mask = mask.cuda()
+
+        with torch.no_grad():
+            prediction = network(image)
+            prediction = torch.sigmoid(prediction)
+
+        # Remove batch dimension.
+        # image = torch.squeeze(image, 0)
+        mask = torch.squeeze(mask, 0)
+        prediction = torch.squeeze(prediction, 0)
+
+        intersection = (prediction * mask).sum()
+        union = prediction.sum() + mask.sum() - intersection
+        iou = intersection / union
+        print(f"Current IoU: {iou}")
+        ious.append(iou.item())
+
+    print(f"IoU: count: {len(ious)}, total: {sum(ious)}, mean: {sum(ious) / len(ious)}")
 
 
 if __name__ == '__main__':
     os.chdir(os.path.dirname(os.path.abspath(__file__)))
     print(f"Working directory: {os.getcwd()}")
 
-    main()
+    # visualize_training_patches()
+    calculate_mean_iou()
